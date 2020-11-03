ea63ff5f4e6b899a5534d9f930d759dd66864ebf
JacksonBey <jacksongrowson@gmail.com>
2020-11-03 15:19:06 -0800

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -4,9 +4,10 @@ import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
   render() {
+    console.log(this.props.pokemons)
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {this.props.pokemons.map((pokemon, idx) => <PokemonCard key={idx} pokemon={pokemon} />)}
       </Card.Group>
     )
   }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -4,17 +4,84 @@ import PokemonForm from './PokemonForm'
 import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
+const URL = 'http://localhost:3000/pokemon'
+
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemons: [],
+    search: ''
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(res => res.json())
+    .then(pokemons => {
+      this.setState({
+        pokemons
+      })
+
+      // console.log('pokemons'),
+      // console.log(pokemons)
+    })
+  }
+
+  handleChange= (e) => {
+    this.setState({
+      search: e.target.value
+    })
+  }
+
+  selectPokemons = (search, pokemons) => {
+    return pokemons.filter(pokemon => pokemon.name.slice(0,search.length).toLowerCase() === search.toLowerCase())
+  }
+
+  addPokemon = (pokemon, e) => {
+    e.preventDefault()
+    fetch((URL), {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: pokemon.name,
+        hp: pokemon.hp,
+        sprites: {
+          front: pokemon.frontUrl,
+          back: pokemon.backUrl
+        }
+      })
+    }).then(res => res.json())
+    .then(pokemon => {
+      this.setState({
+        pokemons: [...this.state.pokemons, pokemon]
+      })
+    })
+
+    // console.log(e)
+    // console.log(pokemon)
+    // "id": 2,
+    // "name": "ivysaur",
+    // "hp": 60,
+    // "sprites": {
+    //   "front": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+    //   "back": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/2.png"
+    // }
+  }
+
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleSubmit={this.addPokemon}/>
         <br />
-        <Search />
+        <Search handleChange={this.handleChange}/>
         <br />
-        <PokemonCollection />
+        {this.state.search === '' ? 
+        <PokemonCollection pokemons={this.state.pokemons}/>
+          : 
+          <PokemonCollection pokemons={this.selectPokemons(this.state.search,this.state.pokemons)}/>
+          }
       </Container>
     )
   }
