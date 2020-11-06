732d269af22c16c5b50230c0e1b8348fd42ec39b
Arpita-22 <duttaarpita22@gmail.com>
2020-11-05 17:29:46 -0800

    Done.

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,49 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  constructor(){
+    super()
+    this.state={
+      pokemons:[],
+      // selectedPokemon:[]
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(data => this.setState({
+      pokemons:data,
+      // selectedPokemon:data
+    }))
+  }
+
+  displayPokemon = (name) =>{
+    this.setState({
+      // selectedPokemons:this.state.pokemons.filter(pokemon => pokemon.name === name)
+      pokemons:[...this.state.pokemons.filter(pokemon => pokemon.name === name)]
+   })
+  }
+
+  addPokemon = (pokemon) =>{
+    console.log(pokemon)
+    this.setState({
+      pokemons:[...this.state.pokemons,pokemon]
+    })
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm addPokemon={this.addPokemon} />
         <br />
-        <Search />
+        <Search  displayPokemon={this.displayPokemon}/> 
         <br />
-        <PokemonCollection />
+        <PokemonCollection  pokemons={this.state.pokemons} />
+        {/* <PokemonCollection  pokemons={this.state.selectedPokemons} /> */}
       </Container>
     )
   }
